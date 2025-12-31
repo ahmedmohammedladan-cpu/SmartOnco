98a9f727c4535b04883dadef5a25d892e1896902
Ahmed Mohammed Ladan <ahmedmohammedladan@gmail.com>
2025-12-31 12:48:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -229,6 +229,7 @@ def api_predict_breast():
 # ==================================================
 # RUN APP
 # ==================================================
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+import os 
+if __name__ == "__main__": 
+    port = int(os.environ.get("PORT", 5000)) 
     app.run(host="0.0.0.0", port=port, debug=True)
